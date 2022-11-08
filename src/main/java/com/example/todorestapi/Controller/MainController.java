1928d4cbf1e78eadbcc50c392553ef6530d9df7c
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2022-11-08 16:17:50 +0900

    タスクリスト一覧のコントローラー更新

--- src/main/java/com/example/todorestapi/Controller/MainController.java
+++ src/main/java/com/example/todorestapi/Controller/MainController.java
@@ -1,10 +1,6 @@
 package com.example.todorestapi.Controller;
 
-import java.util.List;
 
-
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,22 +15,28 @@ public class MainController {
 
     private RestTemplate restTemplate = new RestTemplate();
 
+    String url = "http://localhost:8080/tasks/";
+
     @RequestMapping("/")
     public String index(TaskForm form, Model model) {
-        String url = "http://localhost:8080/tasks/";
-        ResponseEntity<List<TaskEntity>> response = restTemplate.exchange(
+        
+        ResponseEntity<TaskEntity[]> list = restTemplate.getForEntity(url, TaskEntity[].class);
+        TaskEntity[] lists = list.getBody();
+        model.addAttribute("list", lists);
+
+        //これでもjsonのリストを取得できる
+        /*ResponseEntity<List<TaskEntity>> response = restTemplate.exchange(
                 url, HttpMethod.GET, null,
                 new ParameterizedTypeReference<List<TaskEntity>>() {
                 });
         List<TaskEntity> list = response.getBody();
         model.addAttribute("list", list);
+        */
         return "main";
-         
     }
 
     @RequestMapping("/create-task")
     public String createTask(TaskForm form) {
-        String url = "http://localhost:8080/tasks/";
         TaskEntity entity = new TaskEntity();
         entity.setStore(form.getStore());
         entity.setPrice(form.getPrice());
